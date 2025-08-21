a3156bb337ef241a74f85c678a19602b19d87062
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-21 22:49:03 +0800

    20250821 v1

--- hsinfenghuang-not-to-github.io.bat
+++ hsinfenghuang-not-to-github.io.bat
@@ -1,8 +1,12 @@
 cd "C:\Users\chbon\Documents\herokujekyllhuanghsinfeng"
 git init
 git add .
-git commit -m "20250810 v1"
+git commit -m "20250821 v1"
 git remote add origin https://github.com/huanghsinfeng/huanghsinfeng-not-to-github.io.git
 git checkout -b main
-git push -u origin main --force
+git pull origin main --rebase
+git checkout --ours $(git diff --name-only --diff-filter=U)
+git add $(git diff --name-only --diff-filter=U)
+git rebase --continue
+git push -u origin main
 pause
--- huanghsinfeng.github.io.bat
+++ huanghsinfeng.github.io.bat
@@ -1,7 +1,11 @@
 cd "C:\Users\chbon\Documents\herokujekyllhuanghsinfeng\_site"
 git init
 git add .
+<<<<<<< HEAD
 git commit -m "20250819 v1"
+=======
+git commit -m "20250821 v1"
+>>>>>>> 67c43b7 (20250821 v1)
 git remote add origin https://github.com/huanghsinfeng/huanghsinfeng.github.io.git
 git checkout -b main
 git push -u origin main --force
